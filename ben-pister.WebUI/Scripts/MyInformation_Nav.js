c67f414a59d44eea4e744c2ea02a003fb91a5b50
ben.pister <ben.pister@zirmed.com>
2016-11-15 15:01:46 -0500

    normalize endings in pdf file

--- ben-pister.WebUI/Scripts/MyInformation_Nav.js
+++ ben-pister.WebUI/Scripts/MyInformation_Nav.js
@@ -3,11 +3,13 @@
 
     if (jQuery('#' + docId).is(':visible')) {
         jQuery('#' + docId).hide('slow');
+        //jQuery('#' + docId).css('display', 'none');
         jQuery('#' + linkId).html('View Resume');
     }
     else {
         document.getElementById(docId).innerHTML = '<object data="Doc_Resources/Ben_Pister_Resume.pdf" type="application/pdf" width="600" height="800"/>'
         jQuery('#' + docId).show('slow');
+        //jQuery('#' + docId).css('display', 'none');
         jQuery('#' + linkId).html('Close Resume');
     }
 }
