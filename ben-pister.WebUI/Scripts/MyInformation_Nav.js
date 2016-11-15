8cb4757d1e3082f7f3f8faba474b739eb8505d09
ben.pister <ben.pister@zirmed.com>
2016-11-15 14:38:40 -0500

    add resume view

---
+++ ben-pister.WebUI/Scripts/MyInformation_Nav.js
@@ -0,0 +1,13 @@
+﻿function display_document(docId, linkId){
+    
+
+    if (jQuery('#' + docId).is(':visible')) {
+        jQuery('#' + docId).hide('slow');
+        jQuery('#' + linkId).html('View Resume');
+    }
+    else {
+        document.getElementById(docId).innerHTML = '<object data="Doc_Resources/Ben_Pister_Resume.pdf" type="application/pdf" width="600" height="800"/>'
+        jQuery('#' + docId).show('slow');
+        jQuery('#' + linkId).html('Close Resume');
+    }
+}
