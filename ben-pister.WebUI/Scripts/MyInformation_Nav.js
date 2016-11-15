44158139fe07b85b6605d947a44b09109e25a5af
ben.pister <ben.pister@zirmed.com>
2016-11-15 17:10:22 -0500

    change navigation for pdf view

--- ben-pister.WebUI/Scripts/MyInformation_Nav.js
+++ ben-pister.WebUI/Scripts/MyInformation_Nav.js
@@ -1,15 +1,14 @@
-﻿function display_document(docId, linkId){
+﻿var shown = true;
+function display_document(docId, linkId) {
     
-
-    if (jQuery('#' + docId).is(':visible')) {
-        jQuery('#' + docId).hide('slow');
-        //jQuery('#' + docId).css('display', 'none');
+    if (!shown) {
         jQuery('#' + linkId).html('View Resume');
+        document.getElementById(docId).innerHTML = ''
+        shown = true;
     }
     else {
-        document.getElementById(docId).innerHTML = '<object data="Doc_Resources/Ben_Pister_Resume.pdf" type="application/pdf" width="600" height="800"/>'
-        jQuery('#' + docId).show('slow');
-        //jQuery('#' + docId).css('display', 'none');
         jQuery('#' + linkId).html('Close Resume');
+        document.getElementById(docId).innerHTML = '<object data="Doc_Resources/Ben_Pister_Resume.pdf" type="application/pdf" width="600" height="800"/>'
+        shown = false;
     }
 }
