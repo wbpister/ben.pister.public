adeff00fc1d28912a17ae11b4974ff9930e90828
ben.pister <ben.pister@zirmed.com>
2016-11-16 11:23:45 -0500

    adjust UI render for background and resume

---
+++ ben-pister.WebUI/Scripts/Doc_View.js
@@ -0,0 +1,14 @@
+﻿var shown = true;
+function display_document_view(docId, linkId) {
+
+    if (!shown) {
+        jQuery('#' + linkId).html('View Resume');
+        document.getElementById(docId).innerHTML = ''
+        shown = true;
+    }
+    else {
+        jQuery('#' + linkId).html('Close Resume');
+        document.getElementById(docId).innerHTML = '<embed src="Doc_Resources/Ben_Pister_Resume.pdf" type="application/pdf" width="600" height="800"/>'
+        shown = false;
+    }
+}
