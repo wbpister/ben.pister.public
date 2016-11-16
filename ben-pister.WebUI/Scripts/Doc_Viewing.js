2d57cceb9b8c01bd9011408e1bedf53521b8b420
ben.pister <ben.pister@zirmed.com>
2016-11-16 11:57:04 -0500

    delete and re-introduce resume

--- ben-pister.WebUI/Scripts/Doc_Viewing.js
+++ ben-pister.WebUI/Scripts/Doc_Viewing.js
@@ -8,7 +8,7 @@ function display_document(docId, linkId) {
     }
     else {
         jQuery('#' + linkId).html('Close Resume');
-        document.getElementById(docId).innerHTML = '<embed src="Doc_Resources/Ben_Pister_Resume.pdf" type="application/pdf" width="600" height="800"/>'
+        document.getElementById(docId).innerHTML = '<embed src="Doc_Resources/Ben_Pisters_Resume.pdf" type="application/pdf" width="600" height="800"/>'
         shown = false;
     }
 }
