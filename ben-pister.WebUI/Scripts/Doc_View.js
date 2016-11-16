0da503b843ea25568bd2c227a7740224fa95e004
ben.pister <ben.pister@zirmed.com>
2016-11-16 11:33:50 -0500

    render resume on live site fix

--- ben-pister.WebUI/Scripts/Doc_View.js
+++ ben-pister.WebUI/Scripts/Doc_View.js
@@ -3,12 +3,14 @@ function display_document_view(docId, linkId) {
 
     if (!shown) {
         jQuery('#' + linkId).html('View Resume');
-        document.getElementById(docId).innerHTML = ''
+        //document.getElementById(docId).innerHTML = ''
+        jQuery('#' + docId).fadeOut(250);
         shown = true;
     }
     else {
         jQuery('#' + linkId).html('Close Resume');
         document.getElementById(docId).innerHTML = '<embed src="Doc_Resources/Ben_Pister_Resume.pdf" type="application/pdf" width="600" height="800"/>'
+        jQuery('#' +  docId).fadeIn(250)
         shown = false;
     }
 }
