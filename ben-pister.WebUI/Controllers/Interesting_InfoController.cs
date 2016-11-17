bfa31903cbd73af511f19bf0ce3798d88782cf08
ben.pister <ben.pister@zirmed.com>
2016-11-17 17:15:01 -0500

    add additional page for hobbies

---
+++ ben-pister.WebUI/Controllers/Interesting_InfoController.cs
@@ -0,0 +1,16 @@
+﻿using ben_pister.WebUI.Models;
+using System.Web.Mvc;
+
+namespace ben_pister.WebUI.Controllers
+{
+    public class Interesting_InfoController : Controller
+    {
+        // GET: Hobbies
+        public ActionResult Hobbies()
+        {
+            //Eatery_Repository er = new Eatery_Repository();
+
+            return View();
+        }
+    }
+}
---
+++ ben-pister.WebUI/Models/Eatery_Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ben_pister.WebUI.Models
+{
+    public class Eatery_Repository
+    {
+        public string cheddarsMenu;
+
+        public Eatery_Repository()
+        {
+            cheddarsMenu = "http://cheddars.com/location/clarksville-in/";
+        }
+    }
+}
