ffb4439bc10a451b98be2a2893e6ce1db110156d
ben.pister <ben.pister@zirmed.com>
2016-11-18 15:47:26 -0500

    navigation fixes

--- ben-pister.WebUI/Scripts/Doc_View.js
+++ ben-pister.WebUI/Scripts/Doc_View.js
@@ -3,16 +3,14 @@ function display_document_view(docId, linkId) {
 
     if (!shown) {
         jQuery('#' + linkId).html('View Resume');
-        //document.getElementById(docId).innerHTML = ''
-        jQuery('#' + docId).fadeOut(250);
+        document.getElementById(docId).innerHTML = ''
+        jQuery('#' + docId).hide(250);
         shown = true;
-        return;
     }
-    else {
+    else if(shown) {
         jQuery('#' + linkId).html('Close Resume');
-        document.getElementById(docId).innerHTML = '<embed src="Doc_Resources/Ben_Pisters_Resume.pdf" type="application/pdf" width="600" height="800"/>'
-        jQuery('#' +  docId).fadeIn(250)
+        document.getElementById(docId).innerHTML = '<embed src="Doc_Resources/Ben_Pisters_Resume.pdf" type="application/pdf" width="600" height="800"/>';
+        jQuery('#' + docId).show(250);
         shown = false;
-        return;
     }
 }
