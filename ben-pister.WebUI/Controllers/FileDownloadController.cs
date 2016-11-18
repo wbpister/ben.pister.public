4ae044a4dd4b3b6ccfa943c834756d3a8ef5bca8
ben.pister <ben.pister@zirmed.com>
2016-11-18 14:47:46 -0500

    UI updates for readability. Download pdf for mobile

---
+++ ben-pister.WebUI/Controllers/FileDownloadController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace ben_pister.WebUI.Controllers
+{
+    public class FileDownloadController : Controller
+    {
+        // GET: FileDownload
+        public ActionResult PdfDownload()
+        {
+            
+            return File("~/Doc_Resources/Ben_Pisters_Resume.pdf", "application/pdf", "Ben Pisters Resume.pdf");
+        }
+    }
+}
--- ben-pister.WebUI/Scripts/Doc_View.js
+++ ben-pister.WebUI/Scripts/Doc_View.js
@@ -6,11 +6,13 @@ function display_document_view(docId, linkId) {
         //document.getElementById(docId).innerHTML = ''
         jQuery('#' + docId).fadeOut(250);
         shown = true;
+        return;
     }
     else {
         jQuery('#' + linkId).html('Close Resume');
         document.getElementById(docId).innerHTML = '<embed src="Doc_Resources/Ben_Pisters_Resume.pdf" type="application/pdf" width="600" height="800"/>'
         jQuery('#' +  docId).fadeIn(250)
         shown = false;
+        return;
     }
 }
